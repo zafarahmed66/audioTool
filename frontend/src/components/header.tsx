74880f703d6bb54d1b983e4e669cef48370feaa1
Zafar Ahmed <zafarahmed2k24@gmail.com>
2025-02-21 21:51:52 +0530

    feat: complete v1.0

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,15 +1,22 @@
-import { motion } from "motion/react"
+import { motion } from "motion/react";
+
 const Header = () => {
   return (
     <motion.div
-    initial={{ opacity: 0, y: 100 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5, delay: 0.8, ease: "easeOut" }}
-    className="max-w-[400px] space-y-2">
-    <h2 className="text-3xl font-semibold text-primary text-center">  Simplify Your Audio with One Reference Track</h2>
-    <p className="text-sm text-muted-foreground text-center">Enhance Speech turns your recordings into professional studio-quality audio with just one reference track.</p>
-      </motion.div>
-  )
-}
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: 0.8, ease: "easeOut" }}
+      className="max-w-[400px] space-y-2"
+    >
+      <h2 className="text-3xl font-semibold text-primary text-center">
+        Enhance your audio tracks with the power of AI
+      </h2>
+      <p className="text-sm text-muted-foreground text-center">
+        Improve clarity and definition, normalize, and add dynamics to your
+        recordings with our automatic mastering tool
+      </p>
+    </motion.div>
+  );
+};
 
-export default Header
+export default Header;
